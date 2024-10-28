a2cf423370dfb7094525c28f54fd79af4096b246
Anuolu-2020 <Anuolu1645@gmail.com>
2024-10-28 11:11:34 +0100

    chore: updated formatter setting

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -3,10 +3,11 @@ local null_ls = require "null-ls"
 
 local opts = {
   sources = {
-    --null_ls.builtins.formatting.prettierd,
-    --null_ls.builtins.formatting.gofumpt,
-    --null_ls.builtins.formatting.goimports_reviser,
-    --null_ls.builtins.formatting.golines,
+    -- null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.gofumpt,
+    null_ls.builtins.formatting.goimports_reviser,
+    null_ls.builtins.formatting.golines,
+    null_ls.builtins.formatting.hclfmt,
     --null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.mypy.with {
       extra_args = function()
