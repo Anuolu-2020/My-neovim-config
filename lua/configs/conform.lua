8d45fcff65d8cb3e66403ed81d26add301a69a1c
Anuolu-2020 <Anuolu1645@gmail.com>
2025-03-05 14:20:36 +0100

    feat: updated conform config

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,8 +1,8 @@
 local options = {
   formatters_by_ft = {
-    lua = { "stylua" },
-    css = { "cssls" },
-    html = { "prettierd" },
+    -- lua = { "stylua" },
+    css = { "prettier" },
+    html = { "prettier" },
     javascript = { "biome" },
     typescript = { "biome" },
     --go = { "golines", "gofumpt" },
@@ -10,11 +10,13 @@ local options = {
     clang = { "clang_format" },
     json = { "biome" },
     yaml = { "yamlfmt" },
+    templ = { "templ" },
+    asm = { "asmfmt" },
   },
 
   format_on_save = {
     -- These options will be passed to conform.format()
-    --timeout_ms = 500,
+    -- timeout_ms = 500,
     lsp_fallback = true,
   },
 }
