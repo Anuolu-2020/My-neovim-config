db5e5f365a88b30805bd61f64663542b2d34fb54
Anuolu-2020 <Anuolu1645@gmail.com>
2025-03-05 14:22:58 +0100

    chore: improved mappings config

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,114 +2,11 @@ require "nvchad.mappings"
 
 -- add yours here
 
-local M = {}
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+
 map("i", "jk", "<ESC>")
-
-map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
-map("i", "<C-e>", "<End>", { desc = "move end of line" })
-map("i", "<C-h>", "<Left>", { desc = "move left" })
-map("i", "<C-l>", "<Right>", { desc = "move right" })
-map("i", "<C-j>", "<Down>", { desc = "move down" })
-map("i", "<C-k>", "<Up>", { desc = "move up" })
-
-map("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
-
-map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
-map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
-map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
-map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
-
-map("n", "<C-s>", "<cmd>w<CR>", { desc = "file save" })
-map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "file copy whole" })
-
-map("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "toggle line number" })
-map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "toggle relative number" })
-map("n", "<leader>ch", "<cmd>NvCheatsheet<CR>", { desc = "toggle nvcheatsheet" })
-
-map("n", "<leader>fm", function()
-  require("conform").format { lsp_fallback = true }
-end, { desc = "format files" })
-
--- global lsp mappings
-map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "lsp diagnostic loclist" })
-
--- tabufline
-map("n", "<leader>b", "<cmd>enew<CR>", { desc = "buffer new" })
-
-map("n", "<tab>", function()
-  require("nvchad.tabufline").next()
-end, { desc = "buffer goto next" })
-
-map("n", "<S-tab>", function()
-  require("nvchad.tabufline").prev()
-end, { desc = "buffer goto prev" })
-
-map("n", "<leader>x", function()
-  require("nvchad.tabufline").close_buffer()
-end, { desc = "buffer close" })
-
--- Comment
-map("n", "<leader>/", "gcc", { desc = "comment toggle", remap = true })
-map("v", "<leader>/", "gc", { desc = "comment toggle", remap = true })
-
--- nvimtree
-map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
-map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
-
--- telescope
-map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
-map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
-map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "telescope help page" })
-map("n", "<leader>ma", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
-map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "telescope find oldfiles" })
-map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "telescope find in current buffer" })
-map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
-map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
-map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
-map("n", "<leader>th", "<cmd>Telescope themes<CR>", { desc = "telescope nvchad themes" })
-map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
-map(
-  "n",
-  "<leader>fa",
-  "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
-  { desc = "telescope find all files" }
-)
-
--- terminal
-map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
-
--- new terminals
-map("n", "<leader>h", function()
-  require("nvchad.term").new { pos = "sp" }
-end, { desc = "terminal new horizontal term" })
-
-map("n", "<leader>v", function()
-  require("nvchad.term").new { pos = "vsp" }
-end, { desc = "terminal new vertical window" })
-
--- toggleable
-map({ "n", "t" }, "<A-v>", function()
-  require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm" }
-end, { desc = "terminal toggleable vertical term" })
-
-map({ "n", "t" }, "<A-h>", function()
-  require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
-end, { desc = "terminal new horizontal term" })
-
-map({ "n", "t" }, "<A-i>", function()
-  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
-end, { desc = "terminal toggle floating term" })
-
--- whichkey
-map("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
-
-map("n", "<leader>wk", function()
-  vim.cmd("WhichKey " .. vim.fn.input "WhichKey: ")
-end, { desc = "whichkey query lookup" })
 
 -- blankline
 map("n", "<leader>cc", function()
@@ -128,13 +25,27 @@ map("n", "<leader>cc", function()
 end, { desc = "blankline jump to current context" })
 
 -- DAP keymappings
-map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Add breakpoint at line" })
+map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { noremap = true, desc = "Add breakpoint at line" })
+--map("n", "<leader>dui", "<cmd>lua require'dapui'.toggle()<CR>", { desc = "Dap Ui Toggle", noremap = true })
+map("n", "<leader>do", "<cmd> DapStepOut <CR>", { desc = "Step Out" })
+map("n", "<leader>dO", "<cmd> DapStepOver <CR>", { desc = "Step Over" })
+map("n", "<leader>dI", "<cmd> DapStepInto <CR>", { desc = "Step Into" })
+map("n", "<leader>dp", function()
+  require("dap").pause()
+end, { desc = "Pause" })
+map("n", "<leader>dt", "<cmd> DapTerminate <CR>", { desc = "Terminate" })
 map("n", "<leader>dus", function()
   local widgets = require "dap.ui.widgets"
   local sidebar = widgets.sidebar(widgets.scopes)
   sidebar.open()
 end, { desc = "Open debugging sidebar" })
-map("n", "<leader>dr", "<cmd> DapContinue <CR>", { desc = "Start or continue the debugger" })
+map("n", "<leader>dc", "<cmd> DapContinue <CR>", { desc = "Start or continue the debugger" })
+map(
+  "n",
+  "<leader>dr",
+  "<cmd>lua require('dapui').open({reset = true }) <CR>",
+  { desc = "Reset debugger ui", noremap = true }
+)
 
 -- DAP Go keymappings
 map("n", "<leader>dgt", function()
@@ -147,13 +58,14 @@ end, { desc = "Debug last go test" })
 -- Gopher keymappings
 map("n", "<leader>gsj", "<cmd> GoTagAdd json <CR>", { desc = "Add json struct tags" })
 map("n", "<leader>gsy", "<cmd> GoTagAdd yaml <CR>", { desc = "Add yaml struct tags" })
+map("n", "<leader>ge", "<cmd> GoIfErr <CR>", { desc = "Add if error boilerplate" })
 
 -- DAP Python keymappings
 map("n", "<leader>dpr", function()
   require("dap-python").test_method()
 end, { desc = "Test Python method" })
 
--- Lua
+-- trouble keymappings
 map("n", "<leader>oe", function()
   require("trouble").toggle()
 end)
@@ -173,6 +85,10 @@ map("n", "gR", function()
   require("trouble").toggle "lsp_references"
 end)
 
-return M
+map("n", "<leader>tc", function()
+  if vim.bo.filetype == "java" then
+    require("jdtls").test_class()
+  end
+end, { desc = "Run Java test class" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
