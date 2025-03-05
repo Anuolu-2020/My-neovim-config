45c205dbd213deda92e407a08ad3e59a46351a02
Anuolu-2020 <Anuolu1645@gmail.com>
2025-03-05 14:22:04 +0100

    feat: improved plugins init config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,38 +1,86 @@
 return {
   {
-    "nvim-java/nvim-java",
-    -- config = false,
-    dependencies = {
-      {
-        "neovim/nvim-lspconfig",
-        --     opts = {
-        --       servers = {
-        --         jdtls = {
-        --           -- Your custom jdtls settings goes here
-        --         },
-        --       },
-        setup = {
-          jdtls = function()
-            require("java").setup {
-              -- Your custom nvim-java configuration goes here
-            }
-          end,
-        },
-      },
+    "mfussenegger/nvim-jdtls",
+    dependencies = { "folke/which-key.nvim" },
+    ft = "java",
+  },
+  {
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+
+      -- your config goes here
+      -- or just leave it empty :)
     },
   },
   {
-    "stevearc/conform.nvim",
-    event = "BufWritePre", -- uncomment for format on save
-    opts = require "configs.conform",
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      --"leoluz/nvim-dap-go",
+      {
+        "theHamsta/nvim-dap-virtual-text",
+        opts = {},
+      },
+    },
+    config = function()
+      require "configs.js-dap-config"
+      local dapui = require "dapui"
+
+      dapui.setup()
+
+      vim.keymap.set("n", "<leader>dui", require("dapui").toggle)
+    end,
   },
   {
-    "rcarriga/nvim-dap-ui",
+    "theHamsta/nvim-dap-virtual-text",
+    opts = {},
+  },
+  {
+    "leo/nvim-dap-go",
+    ft = "go",
     dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      require("dapui").setup()
+      require("dap-go").setup()
+      require("nvim-dap-virtual-text").setup()
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function(_, _)
+      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      require("dap-python").setup(path)
+      --require("lazy").load { plugins = "dap_python" }
+    end,
+  },
+  -- {
+  --   "dreamsofcode-io/nvim-dap-go",
+  --   ft = "go",
+  --   dependencies = "mfussenegger/nvim-dap",
+  --   config = function(_, opts)
+  --     require("dap-go").setup(opts)
+  --     --require("lazy").load { plugins = "dap_go" }
+  --   end,
+  -- },
+  {
+    "rcarriga/nvim-dap-ui",
+    {
+      "theHamsta/nvim-dap-virtual-text",
+      opts = {},
+    },
+    event = "VeryLazy",
+    opts = {},
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
     config = function()
       local dap = require "dap"
       local dapui = require "dapui"
-      dapui.setup()
       dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open()
       end
@@ -45,6 +93,11 @@ return {
     end,
   },
   {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+  {
     "nvimtools/none-ls.nvim",
     event = "VeryLazy",
     --ft = "go",
@@ -52,125 +105,73 @@ return {
       return require "configs.null-ls"
     end,
   },
+
+  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-      --require "plugins.configs.lspconfig"
-    end,
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = {
-      git = {
-        enable = true,
-      },
-      renderer = {
-        highlight_git = true,
-        icons = {
-          show = {
-            git = true,
-          },
-        },
-      },
-    },
-  },
-  {
-    "mfussenegger/nvim-dap",
-  },
-  {
-    "dreamsofcode-io/nvim-dap-go",
-    ft = "go",
-    dependencies = "mfussenegger/nvim-dap",
-    config = function(_, opts)
-      require("dap-go").setup(opts)
-      --require("lazy").load { plugins = "dap_go" }
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap-python",
-    ft = "python",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      "rcarriga/nvim-dap-ui",
-    },
-    config = function(_, opts)
-      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-      require("dap-python").setup(path)
-      --require("lazy").load { plugins = "dap_python" }
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
-    opts = function()
-      return require "nvchad.configs.mason"
-    end,
-  },
-  {
-    --Debugger configuration for c++ using clldb
-    "jay-babu/mason-nvim-dap.nvim",
     event = "VeryLazy",
     dependencies = {
-      "williamboman/mason.nvim",
-      "mfussenegger/nvim-dap",
-    },
-    opts = {
-      handlers = {},
-    },
-  },
-  {
-    "windwp/nvim-ts-autotag",
-    ft = {
-      "javascript",
-      "javascriptreact",
-      "typescript",
-      "typescriptreact",
-      "html",
+
+      -- https://github.com/williamboman/mason.nvim
+      { "williamboman/mason.nvim" },
+
+      -- https://github.com/williamboman/mason-lspconfig.nvim
+      { "williamboman/mason-lspconfig.nvim" },
+
+      -- Auto-Install LSPs, linters, formatters, debuggers
+      -- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
+      { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+
+      { "j-hui/fidget.nvim", opts = {} },
     },
     config = function()
-      require("nvim-ts-autotag").setup()
+      require("mason").setup()
+      require("mason-lspconfig").setup {
+        -- Install these LSPs automatically
+        ensure_installed = {
+          "gradle_ls",
+          "jdtls",
+          "zls",
+          "htmx",
+          "yamlls",
+          "html",
+        },
+        automatic_installation = true,
+      }
+
+      require "configs.lspconfig"
+
+      require("mason-tool-installer").setup {
+        -- Install these linters, formatters, debuggers automatically
+        ensure_installed = {
+          "java-debug-adapter",
+          "js-debug-adapter",
+          "java-test",
+          "delve",
+          "debugpy",
+        },
+      }
     end,
   },
   {
+    -- {
     "nvim-treesitter/nvim-treesitter",
-    opts = function()
-      opts = require "configs.treesitter"
-      require("nvim-treesitter.install").compilers = { "clang" }
-      opts.ensure_installed = {
-        "html",
-        "lua",
-        "typescript",
-        "go",
-        "javascript",
-        "java",
-      }
-    end,
-  },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup {
-        suggestion = {
-          enabled = false,
-          auto_trigger = false,
-        },
-        filetypes = {
-          gitcommit = true,
-          markdown = true,
-        },
-      }
-    end,
-  },
-  {
-    "okuuva/auto-save.nvim",
-    cmd = "ASToggle", -- optional for lazy loading on command
-    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
     opts = {
-      -- your config goes here
-      -- or just leave it empty :)
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "go",
+        "typescript",
+        "zig",
+        "asm",
+        "python",
+        "java",
+        "yaml",
+        "templ",
+      },
     },
   },
   {
@@ -204,4 +205,51 @@ return {
     cmd = { "LiveServerStart", "LiveServerStop" },
     config = true,
   },
+  {
+    --Debugger configuration for c++ using clldb
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+    },
+    cmd = { "DapInstall", "DapUninstall" },
+    ensure_installed = {
+      -- Update this to ensure that you have the debuggers for the langs you want
+    },
+    config = function() end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "javascript",
+      "javascriptreact",
+      --"typescript",
+      "typescriptreact",
+      "html",
+    },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  --[[{
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup {
+        suggestion = {
+          enabled = false,
+          auto_trigger = false,
+        },
+        filetypes = {
+          gitcommit = true,
+          markdown = true,
+        },
+      }
+    end,
+  },--]]
 }
