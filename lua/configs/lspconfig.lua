5c6da1ca93bb415ca8573bc33171c9d6f36b0128
Anuolu-2020 <Anuolu1645@gmail.com>
2025-03-05 14:20:59 +0100

    feat: improved lspconfig file

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,104 +1,14 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-local util = require "lspconfig/util"
+local util = require "lspconfig.util"
+
 local nvlsp = require "nvchad.configs.lspconfig"
 
 --Auto save group
 local group = vim.api.nvim_create_augroup("autosave", {})
 
-local M = {}
 local map = vim.keymap.set
-
--- EXAMPLE
-local servers =
-  { "cssls", "ts_ls", "gopls", "pyright", "ruff_lsp", "htmx", "tailwindcss", "zls", "biome", "yamlls", "jdtls" }
-
---"eslint",
-
--- export on_attach & capabilities
-M.on_attach = function(_, bufnr)
-  local function opts(desc)
-    return { buffer = bufnr, desc = "LSP " .. desc }
-  end
-
-  map("n", "gD", vim.lsp.buf.declaration, opts "Go to declaration")
-  map("n", "gd", vim.lsp.buf.definition, opts "Go to definition")
-  map("n", "gi", vim.lsp.buf.implementation, opts "Go to implementation")
-  map("n", "<leader>sh", vim.lsp.buf.signature_help, opts "Show signature help")
-  map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "Add workspace folder")
-  map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "Remove workspace folder")
-
-  map("n", "<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, opts "List workspace folders")
-
-  map("n", "<leader>D", vim.lsp.buf.type_definition, opts "Go to type definition")
-
-  map("n", "<leader>ra", function()
-    require "nvchad.lsp.renamer"()
-  end, opts "NvRenamer")
-
-  map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts "Code action")
-  map("n", "gr", vim.lsp.buf.references, opts "Show references")
-end
-
--- disable semanticTokens
-M.on_init = function(client, _)
-  if client.supports_method "textDocument/semanticTokens" then
-    client.server_capabilities.semanticTokensProvider = nil
-  end
-end
-
-M.capabilities = vim.lsp.protocol.make_client_capabilities()
-
-M.capabilities.textDocument.completion.completionItem = {
-  documentationFormat = { "markdown", "plaintext" },
-  snippetSupport = true,
-  preselectSupport = true,
-  insertReplaceSupport = true,
-  labelDetailsSupport = true,
-  deprecatedSupport = true,
-  commitCharactersSupport = true,
-  tagSupport = { valueSet = { 1 } },
-  resolveSupport = {
-    properties = {
-      "documentation",
-      "detail",
-      "additionalTextEdits",
-    },
-  },
-}
-
-M.defaults = function()
-  dofile(vim.g.base46_cache .. "lsp")
-  require("nvchad.lsp").diagnostic_config()
-
-  require("lspconfig").lua_ls.setup {
-    on_attach = M.on_attach,
-    capabilities = M.capabilities,
-    on_init = M.on_init,
-
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { "vim" },
-        },
-        workspace = {
-          library = {
-            vim.fn.expand "$VIMRUNTIME/lua",
-            vim.fn.expand "$VIMRUNTIME/lua/vim/lsp",
-            vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
-            vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
-          },
-          maxPreload = 100000,
-          preloadFileSize = 10000,
-        },
-      },
-    },
-  }
-end
 
 local function organize_imports()
   local params = {
@@ -108,147 +18,207 @@ local function organize_imports()
   vim.lsp.buf.execute_command(params)
 end
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+local function go_to_source_definition()
+  local params = vim.lsp.util.make_position_params()
+  params.command = "typescript.goToSourceDefinition"
+  params.arguments = { params.textDocument.uri, params.position }
+  params.open = true
 end
 
-lspconfig.ts_ls.setup {
-  filetypes = { "ts", "js", "html" },
-  commands = {
-    OrganizeImports = {
-      organize_imports,
-      description = "Organize Imports",
-    },
-  },
-  -- on_attach = function()
-  --   vim.api.nvim_create_autocmd("User", {
-  --     group = group,
-  --     --vim.api.nvim_create_augroup("ts_imports", { clear = true }),
-  --     pattern = "AutoSaveWritePost",
-  --     callback = function()
-  --       vim.lsp.buf.code_action {
-  --         apply = true,
-  --         context = {
-  --           commands = organize_imports,
-  --           -- only = { "typescript.organizeImports" },
-  --           diagnostics = {},
+local function findAllFileReferences()
+  local params = {
+    command = "typescript.findAllFileReferences",
+    arguments = { vim.uri_from_bufnr(0) },
+    open = true,
+  }
+  vim.lsp.buf.execute_command(params)
+end
+
+-- EXAMPLE
+local servers = {
+  html = {},
+  cssls = {},
+  -- vtsls = {
+  --   -- explicitly add default filetypes, so that we can extend
+  --   -- them in related extras
+  --   filetypes = {
+  --     "javascript",
+  --     "javascriptreact",
+  --     "javascript.jsx",
+  --     "typescript",
+  --     "typescriptreact",
+  --     "typescript.tsx",
+  --   },
+  --   settings = {
+  --     complete_function_calls = true,
+  --     vtsls = {
+  --       enableMoveToFileCodeAction = true,
+  --       autoUseWorkspaceTsdk = true,
+  --       experimental = {
+  --         maxInlayHintLength = 30,
+  --         completion = {
+  --           enableServerSideFuzzyMatch = true,
   --         },
-  --       }
-  --     end,
-  --   })
-  -- end,
-}
-
-lspconfig.gopls.setup {
-  cmd = { "gopls" },
-  filetypes = { "go", "gomod", "gowork", "gotmpl", "template" },
-  root_dir = util.root_pattern("go.mod", ".git", "go.work"),
-  settings = {
-    gopls = {
-      completeUnimported = true,
-      usePlaceholders = true,
-      analyses = {
-        unusedparams = true,
+  --       },
+  --     },
+  --     typescript = {
+  --       updateImportsOnFileMove = { enabled = "always" },
+  --       suggest = {
+  --         completeFunctionCalls = true,
+  --       },
+  --       inlayHints = {
+  --         enumMemberValues = { enabled = true },
+  --         functionLikeReturnTypes = { enabled = true },
+  --         parameterNames = { enabled = "literals" },
+  --         parameterTypes = { enabled = true },
+  --         propertyDeclarationTypes = { enabled = true },
+  --         variableTypes = { enabled = false },
+  --       },
+  --     },
+  --   },
+  -- },
+  ts_ls = {
+    filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact", "html" },
+    commands = {
+      OrganizeImports = {
+        organize_imports,
+        description = "Organize Typescript Imports",
       },
-      staticcheck = true,
     },
+    on_attach = function(_, bufnr)
+      map("n", "gD", go_to_source_definition, {
+        buffer = bufnr,
+        noremap = true,
+        silent = true,
+        desc = "Goto Source Definition",
+      })
+
+      map("n", "gR", findAllFileReferences, {
+        buffer = bufnr,
+        noremap = true,
+        silent = true,
+        desc = "File References",
+      })
+
+      -- -- vim.notify("AutoSave presave", vim.log.levels.INFO)
+    end,
+    root_dir = function(fname)
+      return util.root_pattern ".git"(fname)
+        or util.root_pattern("package.json", "tsconfig.json", "jsconfig.json")(fname)
+    end,
   },
-  on_attach = function()
-    -- Trigger the organize imports action after the auto-save plugin saves the file
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "AutoSaveWritePost", -- Hook into the auto-save event
-      --buffer = bufnr,
-      group = group,
-      callback = function()
-        vim.lsp.buf.code_action {
-          context = {
-            only = { "source.organizeImports" },
-            diagnostics = {},
-          },
-          apply = true,
-        }
-      end,
-    })
-  end,
-}
+  gopls = {
+    cmd = { "gopls" },
+    filetypes = { "go", "gomod", "gowork", "gotmpl", "template" },
+    root_dir = util.root_pattern("go.mod", ".git", "go.work"),
+    settings = {
+      gopls = {
+        completeUnimported = true,
+        usePlaceholders = true,
+        analyses = {
+          unusedparams = true,
+        },
+        staticcheck = true,
+      },
+    },
+    on_attach = function()
+      -- Trigger the organize imports action after the auto-save plugin saves the file
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "AutoSaveWritePost", -- Hook into the auto-save event
+        -- buffer = bufnr,
+        group = group,
+        callback = function()
+          vim.lsp.buf.code_action {
+            context = {
+              only = { "source.organizeImports" },
+              diagnostics = {},
+            },
+            apply = true,
+          }
+          vim.lsp.buf.code_action {
+            context = { only = { "source.fixAll" }, diagnostics = {} },
+            apply = true,
+          }
 
-lspconfig.yamlls.setup {
-  filetypes = { "yaml", "yml" },
-}
-
-lspconfig.pyright.setup {
-  filetypes = { "python" },
-}
-
---linting for python
-lspconfig.ruff_lsp.setup {
-  filetypes = { "python" },
-}
-
-lspconfig.tailwindcss.setup {
-  filetypes = { "css", "tmpl", "html" },
-}
-
-lspconfig.htmx.setup {
-  filetypes = { "html" },
-}
-
-lspconfig.zls.setup {
-  filetypes = { "zig" },
-}
-
-lspconfig.biome.setup {
-  filetypes = {
-    "javascript",
-    "javascriptreact",
-    "json",
-    "jsonc",
-    "typescript",
-    "typescript.tsx",
-    "typescriptreact",
-    "astro",
-    "svelte",
-    "vue",
-    "css",
+          -- vim.notify("AutoSave presave", vim.log.levels.INFO)
+        end,
+      })
+    end,
   },
-  root_dir = util.root_pattern("biome.json", "biome.jsonc"),
-  single_file_support = false,
-}
-
-lspconfig.clangd.setup {
-  on_attach = function(client, bufnr)
-    client.server_capabilities.signatureHelpProvider = false
-    nvlsp.on_attach(client, bufnr)
-  end,
-  capabilities = nvlsp.capabilities,
-}
-
-lspconfig.jdtls.setup {
-  settings = {
-    java = {
-      configuration = {
-        runtimes = {
-          {
-            name = "JavaSE-21",
-            path = "/usr/lib/jvm/jdk-21.0.5-oracle-x64",
-            default = true,
-          },
+  pyright = {
+    filetypes = { "python" },
+    settings = {
+      pyright = {
+        disableOrganizeImports = false,
+        analysis = {
+          useLibraryCodeForTypes = true,
+          autoSearchPaths = true,
+          diagnosticMode = "workspace",
+          autoImportCompletions = true,
         },
       },
     },
   },
-
-  filetypes = { "java" },
+  --linting for python
+  ruff = {
+    filetypes = { "python" },
+  },
+  htmx = {
+    filetypes = { "html" },
+  },
+  tailwindcss = {
+    filetypes = { "css", "tmpl", "html" },
+  },
+  zls = {
+    filetypes = { "zig" },
+  },
+  biome = {
+    filetypes = {
+      "javascript",
+      "javascriptreact",
+      "json",
+      "jsonc",
+      "typescript",
+      "typescript.tsx",
+      "typescriptreact",
+      "astro",
+      "svelte",
+      "vue",
+      "css",
+    },
+    root_dir = util.root_pattern("biome.json", "biome.jsonc"),
+    single_file_support = false,
+  },
+  yamlls = {
+    filetypes = { "yaml", "yml" },
+  },
+  -- jdtls = {},
+  -- lua_ls = {
+  --   filetypes = { "lua" },
+  -- },
+  templ = {
+    filetypes = { "templ" },
+  },
+  emmet_ls = {
+    filetypes = { "templ", "html" },
+  },
+  asm_lsp = {
+    filetypes = { "asm" },
+  },
+  clangd = {
+    on_attach = function(client, _)
+      client.server_capabilities.signatureHelpProvider = false
+    end,
+  },
 }
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+for name, opts in pairs(servers) do
+  opts.on_init = nvlsp.on_init
+  -- Only set default on_attach if a custom one isn't already provided
+  if not opts.on_attach then
+    opts.on_attach = nvlsp.on_attach
+  end
+  opts.capabilities = nvlsp.capabilities
+
+  require("lspconfig")[name].setup(opts)
+end
