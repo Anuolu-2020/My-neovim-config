ed3112a6f2f05b946336e0e6865f5abcaa2c5bd4
Anuolu-2020 <Anuolu1645@gmail.com>
2025-03-26 12:07:31 +0100

    feat: nvim surround plugin added

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -235,6 +235,16 @@ return {
       require("nvim-ts-autotag").setup()
     end,
   },
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
   --[[{
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
