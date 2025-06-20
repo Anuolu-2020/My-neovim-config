dea962cef18b1a3b502f9aaeafe03aa960e7ab55
Anuolu-2020 <Anuolu1645@gmail.com>
2025-06-20 14:11:13 +0100

    chore: display nvchad logo when there's no buffer

--- lua/options.lua
+++ lua/options.lua
@@ -18,3 +18,12 @@ vim.fn.sign_define("DapStopped", {
   linehl = "DapBreakpoint",
   numhl = "DapBreakpoint",
 })
+
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+      vim.cmd "Nvdash"
+    end
+  end,
+})
